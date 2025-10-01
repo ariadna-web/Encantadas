4e01063f46406a4f2fa8941d3f00825392a507cf
Equis-neutrx <ariadnagermano140@gmail.com>
2025-09-30 22:35:49 -0300

    _

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.use(express.static('carrito'));
 
 
 // Conexión MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Conectado a MongoDB'))
 .catch(err => console.error('Error conectando a MongoDB', err));
 
